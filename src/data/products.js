9467f34de6e9335d191afec634bf66ea34d7673c
emiltriest <emil@triest.de>
2023-02-02 15:34:12 -0400

    initial commit

---
+++ src/data/products.js
@@ -0,0 +1,57 @@
+export const products = [
+    {
+        "product": "T-Shirt",
+        "description": "A T-shirt, or tee shirt, is a style of fabric shirt named after the T shape of its body and sleeves. Traditionally, it has short sleeves and a round neckline, known as a crew neck, which lacks a collar. T-shirts are generally made of a stretchy, light, and inexpensive fabric and are easy to clean.",
+        "image": "tshirt.jpeg",
+        "price": 497
+    },
+    {
+        "product": "Hoodie",
+        "description": "A hoodie (in some cases it is also spelt hoody and alternatively known as a hooded sweatshirt) is a sweatshirt with a hood. Hoodies often include a muff sewn onto the lower front, and (usually) a drawstring to adjust the hood opening.",
+        "image": "hoodie.jpeg",
+        "price": 528
+    },
+    {
+        "product": "Trousers",
+        "description": "Trousers (British English), slacks, or pants are an item of clothing that might have originated in Central Asia, worn from the waist to the ankles, covering both legs separately (rather than with cloth extending across both legs as in robes, skirts, and dresses).",
+        "image": "trousers.jpeg",
+        "price": 1342
+    },
+    {
+        "product": "Sunglasses",
+        "description": "Sunglasses or sun glasses (informally called shades or sunnies; more names below) are a form of protective eyewear designed primarily to prevent bright sunlight and high-energy visible light from damaging or discomforting the eyes.",
+        "image": "sunglasses.jpg",
+        "price": 4545
+    },
+
+    {
+        "product": "Necklace",
+        "description": "A necklace is an article of jewellery that is worn around the neck. Necklaces may have been one of the earliest types of adornment worn by humans. They often serve ceremonial, religious, magical, or funerary purposes and are also used as symbols of wealth and status, given that they are commonly made of precious metals and stones.",
+        "image": "necklace.jpeg",
+        "price": 7742
+    },
+    {
+        "product": "Basketball",
+        "description": "A basketball is a spherical ball used in basketball games. Basketballs usually range in size from very small promotional items that are only a few inches (some centimeters) in diameter to extra large balls nearly 2 feet (60 cm) in diameter used in training exercises.",
+        "image": "basketball.jpeg",
+        "price": 6581
+    },
+    {
+        "product": "Jacket",
+        "description": "A jacket is a garment for the upper body, usually extending below the hips. A jacket typically has sleeves, and fastens in the front or slightly on the side. A jacket is generally lighter, tighter-fitting, and less insulating than a coat, which is outerwear.",
+        "image": "canadagoose.jpg",
+        "price": 94998
+    },
+    {
+        "product": "Gold Bar",
+        "description": "A gold bar, also called gold bullion or gold ingot, is a quantity of refined metallic gold of any shape that is made by a bar producer meeting standard conditions of manufacture, labeling, and record keeping.",
+        "image": "goldbar.jpg",
+        "price": 110040
+    },
+    {
+        "product": "Hamburger",
+        "description": "A hamburger (or burger for short) is a food, typically considered a sandwich, consisting of one or more cooked patties—usually ground meat, typically beef—placed inside a sliced bread roll or bun.",
+        "image": "hamburger.jpeg",
+        "price": 399
+    }
+]
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,70 @@
-import '@/styles/globals.css'
+import {AppProps} from 'next/app';
+import Head from 'next/head';
+import {MantineProvider, Header, Group, Text, SimpleGrid, Button, ThemeIcon, Drawer} from '@mantine/core';
+import {IconShoppingCart} from "@tabler/icons-react";
+import Link from "next/link";
+import styles from "@/styles/Globals.css";
+import {useState} from "react";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App(props) {
+    const {Component, pageProps} = props;
+
+    const [cartOpen, setCartOpen] = useState(false);
+
+    return (
+        <>
+            <Head>
+                <title>Page title</title>
+                <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width"/>
+            </Head>
+
+            <MantineProvider
+                withGlobalStyles
+                withNormalizeCSS
+                theme={{
+                    /** Put your mantine theme override here */
+                    colorScheme: 'light',
+                    colors: {
+                        'dark': [
+                            '#F4FCE3',
+                            '#E9FAC8',
+                            '#D8F5A2',
+                            '#C0EB75',
+                            '#A9E34B',
+                            '#94D82D',
+                            '#82C91E',
+                            '#74B816',
+                            '#66A80F',
+                            '#5C940D',
+                        ]
+                    }
+                }}
+            >
+                <Header p="sm">
+                    <Group position="apart">
+                        <div></div>
+                        <Link href="/">
+                            <Group style={{cursor: "pointer", textDecoration: "none"}}>
+                                <Text size="lg" weight={300}>Choose.</Text>
+                                <Text size="lg" weight={300}>Click.</Text>
+                                <Text size="lg" weight={300}>Order.</Text>
+                            </Group>
+                        </Link>
+                        <div style={{paddingRight: "1%"}}>
+                            <ThemeIcon radius="xl" size="lg" style={{cursor: "pointer"}} onClick={() => {
+                                setCartOpen(true)
+                            }}
+                                       variant="gradient" gradient={{from: 'teal', to: 'lime', deg: 60}}>
+                                <IconShoppingCart size="22"/>
+                            </ThemeIcon>
+                        </div>
+                    </Group>
+                </Header>
+                <Drawer opened={cartOpen} onClose={() => setCartOpen(false)}
+                        size="xl" position="right" overlayBlur={3} overlayOpacity={0.3} withCloseButton={false}>
+                </Drawer>
+                <Component {...pageProps} setCartOpen={setCartOpen}/>
+            </MantineProvider>
+        </>
+    );
 }
